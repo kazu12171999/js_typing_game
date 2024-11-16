e5ea9060ac5c11925b2edbb14e5583ea40322b84
kazuhide <kazuhide12171999@gmail.com>
2024-11-16 12:21:22 +0900

    1文字の正誤判定

--- script.js
+++ script.js
@@ -9,4 +9,18 @@ const  question = [
     'addEventListener'
 ];
 
-console.log(question);
+// console.log(question);
+
+const entered = document.getElementById('enterhed');
+const remained = document.getElementById('remained');
+const inputText = document.getElementById('inputText');
+
+let remainedTextWords = remained.textContent.split('');
+
+document.addEventListener('input', (e) => {
+    if(remainedTextWords[0] === e.data){
+        console.log('正解！');
+    }else{
+        console.log('不正解');
+    }
+});
